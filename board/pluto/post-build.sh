7c7d1542a718b95b92abebff118529751713ea44
Manawyrm <git@tbspace.de>
2026-06-26 21:44:25 +0200

    add osmo-trx config, autostart

--- board/pluto/post-build.sh
+++ board/pluto/post-build.sh
@@ -79,6 +79,7 @@ ${INSTALL} -D -m 0644 ${BOARD_DIR}/mdev.conf ${TARGET_DIR}/etc/
 ${INSTALL} -D -m 0755 ${BOARD_DIR}/automounter.sh ${TARGET_DIR}/lib/mdev/automounter.sh
 ${INSTALL} -D -m 0755 ${BOARD_DIR}/ifupdown.sh ${TARGET_DIR}/lib/mdev/ifupdown.sh
 ${INSTALL} -D -m 0644 ${BOARD_DIR}/input-event-daemon.conf ${TARGET_DIR}/etc/
+${INSTALL} -D -m 0644 ${BOARD_DIR}/osmo-trx-pluto.cfg ${TARGET_DIR}/etc/
 
 ${INSTALL} -D -m 0644 ${BOARD_DIR}/msd/img/* ${TARGET_DIR}/www/img/
 ${INSTALL} -D -m 0644 ${BOARD_DIR}/msd/*.html ${TARGET_DIR}/www/
